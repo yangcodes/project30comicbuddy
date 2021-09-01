35987507fd6d00cd106d5ef7c8daf033c7b75644
Jenny Lin <misslinyang91@gmail.com>
2021-09-02 09:02:02 +1000

    created the superhero entry class

---
+++ app.js
@@ -0,0 +1,8 @@
+// the superhero entry
+class SuperheroEntry {
+  constructor(superheroName, superheroUniverse, superheroPower) {
+    this.superheroName = superheroName;
+    this.superheroUniverse = superheroUniverse;
+    this.superheroPower = superheroPower;
+  }
+}
